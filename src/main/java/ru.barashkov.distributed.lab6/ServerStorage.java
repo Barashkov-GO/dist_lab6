ec2edb566f9026cbd8270e162ba2801fe79e3b18
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-25 02:52:24 +0300

    Scripted auto-commit on change (2021-12-25 02:52:24) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab6/ServerStorage.java
+++ src/main/java/ru.barashkov.distributed.lab6/ServerStorage.java
@@ -2,6 +2,8 @@ package ru.barashkov.distributed.lab6;
 
 import akka.actor.ActorRef;
 import akka.http.javadsl.Http;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
 
 public class ServerStorage {
@@ -16,5 +18,9 @@ public class ServerStorage {
         this.actorStorage = actorStorage;
         this.zooKeeper = zooKeeper;
         this.way = URL + port;
+        zoo.create("/servers/" + way,
+                way.getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                CreateMode.EPHEMERAL_SEQUENTIAL);
     }
 }
