7f3eda88ab5d98f5b1c01a3c4f955b79704dd293
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-25 02:20:12 +0300

    Scripted auto-commit on change (2021-12-25 02:20:12) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab6/ZookeeperApp.java
+++ src/main/java/ru.barashkov.distributed.lab6/ZookeeperApp.java
@@ -1,13 +1,16 @@
 package ru.barashkov.distributed.lab6;
 
 
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.stream.ActorMaterializer;
 
 public class ZookeeperApp {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("routes");
-        ActorRef storage = system.actorOf(Props.create(StorageActor.class));
+        ActorRef storage = system.actorOf(Props.create(ActorStorage.class));
         final ActorMaterializer materializer = ActorMaterializer.create(system);
+        
     }
 }
