45830fda79ac953ff228b1068a86b51355f5151c
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-25 02:06:03 +0300

    Scripted auto-commit on change (2021-12-25 02:06:03) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab6/ActorStorage.java
+++ src/main/java/ru.barashkov.distributed.lab6/ActorStorage.java
@@ -24,7 +24,7 @@ public class StorageActor extends AbstractActor {
                 ).
                 match(
                         MessageGetList.class,
-                        message -> servers = 
+                        message -> servers = message.get
                 )
     }
 
