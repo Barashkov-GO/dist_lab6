f152b35f4bba391a33baf48205e8e9878a59df00
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-25 02:00:28 +0300

    Scripted auto-commit on change (2021-12-25 02:00:28) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab6/ActorStorage.java
+++ src/main/java/ru.barashkov.distributed.lab6/ActorStorage.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class StorageActor extends AbstractActor {
+    private List<String> servers = new ArrayList<>();
+    private final Random random = new Random();
 
 }
