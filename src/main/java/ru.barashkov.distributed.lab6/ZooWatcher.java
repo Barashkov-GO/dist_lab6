5d462170b9e1bf7e2e4562d72e2c4d563197d42e
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-25 02:39:03 +0300

    Scripted auto-commit on change (2021-12-25 02:39:03) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab6/ZooWatcher.java
+++ src/main/java/ru.barashkov.distributed.lab6/ZooWatcher.java
@@ -22,6 +22,6 @@ public class ZooWatcher implements Watcher {
 
     private void sendAnswer() {
         List<String> servers = new ArrayList<>();
-        for (String s : )
+        for (String s : zooKeeper.getChildren)
     }
 }
