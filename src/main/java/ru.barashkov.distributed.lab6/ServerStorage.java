480ff5e0b64b92da753d5e1dc07d38368fe6c8cc
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-25 02:50:54 +0300

    Scripted auto-commit on change (2021-12-25 02:50:54) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab6/ServerStorage.java
+++ src/main/java/ru.barashkov.distributed.lab6/ServerStorage.java
@@ -5,8 +5,15 @@ import akka.http.javadsl.Http;
 import org.apache.zookeeper.ZooKeeper;
 
 public class ServerStorage {
+    private Http http;
+    private ActorRef actorStorage;
+    private ZooKeeper zooKeeper;
+    private String way;
 
     public ServerStorage(Http http, ActorRef actorStorage, ZooKeeper zooKeeper, String way) {
-        
+        this.http = http;
+        this.actorStorage = actorStorage;
+        this.zooKeeper = zooKeeper;
+        this.way = way;
     }
 }
