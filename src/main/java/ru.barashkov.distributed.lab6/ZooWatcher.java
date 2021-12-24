ea71d0fe2cab95ba39d4328f62516ab165102052
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-25 02:36:24 +0300

    Scripted auto-commit on change (2021-12-25 02:36:24) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab6/ZooWatcher.java
+++ src/main/java/ru.barashkov.distributed.lab6/ZooWatcher.java
@@ -4,9 +4,10 @@ import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 
 public class ZooWatcher implements Watcher {
+    private static final String SERVERS_PATH = "/servers";
 
     @Override
     public void process(WatchedEvent watchedEvent) {
-
+        
     }
 }
