778c700712dd299628bef8ba0140c614384adccb
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-25 03:06:30 +0300

    Scripted auto-commit on change (2021-12-25 03:06:30) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab6/ServerStorage.java
+++ src/main/java/ru.barashkov.distributed.lab6/ServerStorage.java
@@ -29,7 +29,7 @@ public class ServerStorage implements Watcher {
     public Route createRoute() {
         return route(
                 path(
-                        way, () -> 
+                        way, () -> route
                 )
 
         )
